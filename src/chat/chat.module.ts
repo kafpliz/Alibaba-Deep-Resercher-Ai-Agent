d96c55178c55b331a3e53f07d75ebb50e1c1fa23
kafpliz <kafpliz@gmail.com>
2025-10-26 19:12:12 +0300

    Edit chatModule

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { HistoryService } from 'src/shared/entity/history/history.service';
 import { HistoryModule } from 'src/shared/entity/history/history.module';
 import { UserModule } from 'src/shared/entity/user/user.module';
 
